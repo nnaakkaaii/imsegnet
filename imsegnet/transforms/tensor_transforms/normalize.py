cd3cfda71224cb0f86a0e83c2f7dc0366ec744a5
nnaakkaaii <ultraganbayu10@gmail.com>
2023-03-11 20:52:18 +0900

    add normalize transform

---
+++ imsegnet/transforms/tensor_transforms/normalize.py
@@ -0,0 +1,42 @@
+from torch import Tensor
+from torchvision import transforms
+
+from .interface import Interface
+
+
+class Normalize(Interface):
+    MEAN = (0.5,)
+    STD = (0.5,)
+
+    def __init__(self):
+        self.normalize = transforms.Normalize(self.MEAN, self.STD)
+        self.unnormalize = transforms.Compose([
+            transforms.Normalize((0,), tuple(1/i for i in self.STD)),
+            transforms.Normalize(tuple(-i for i in self.MEAN), (1,)),
+            ])
+
+    def forward(self, x: list[Tensor]) -> list[Tensor]:
+        return [self.normalize(_x) for _x in x]
+
+    def backward(self, x: list[Tensor]) -> list[Tensor]:
+        return [self.unnormalize(_x) for _x in x]
+
+
+if __name__ == "__main__":
+    # python3 -m imsegnet.transforms.tensor_transforms.normalize
+    from ...datasets.moving_mnist import MovingMNIST
+
+    s = MovingMNIST(root_dir="./data",
+                    pil_transforms=[],
+                    tensor_transforms=[],
+                    )
+    d, _ = next(iter(s))
+    print(d.shape)
+    print(d.min(), d.max())
+    s = MovingMNIST(root_dir="./data",
+                    pil_transforms=[],
+                    tensor_transforms=[Normalize()],
+                    )
+    d, _ = next(iter(s))
+    print(d.shape)
+    print(d.min(), d.max())
