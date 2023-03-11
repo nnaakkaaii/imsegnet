937dffc481a1548d6b1db90fcf947fb068804797
nnaakkaaii <ultraganbayu10@gmail.com>
2023-03-12 00:49:01 +0900

    add random flip transform

---
+++ imsegnet/transforms/tensor_transforms/random_flip.py
@@ -0,0 +1,21 @@
+import random
+
+from torch import Tensor
+from torchvision.transforms import functional as tf
+
+from .interface import Interface
+
+
+class RandomFlip(Interface):
+    def forward(self, x: list[Tensor]) -> list[Tensor]:
+        if random.random() > 0.5:
+            return [tf.hflip(x_) for x_ in x]
+        return x
+
+
+if __name__ == "__main__":
+    # python3 -m imsegnet.transforms.tensor_transforms.random_flip
+    from ...datasets.moving_mnist import MovingMNIST
+    s = MovingMNIST(root_dir="./data", tensor_transforms=[RandomFlip()])
+    d, _ = next(iter(s))
+    print(d.shape)
