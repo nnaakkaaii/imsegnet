2c7d65078ae26ac68e019487d43034522b733a50
nnaakkaaii <ultraganbayu10@gmail.com>
2023-03-12 00:48:48 +0900

    add random affine crop

---
+++ imsegnet/transforms/tensor_transforms/random_crop.py
@@ -0,0 +1,21 @@
+from torch import Tensor
+from torchvision import transforms
+from torchvision.transforms import functional as tf
+
+from .interface import Interface
+
+
+class RandomCrop(Interface):
+    def forward(self, x: list[Tensor]) -> list[Tensor]:
+        _, w, h = x[0].size()
+        assert w == h
+        p = transforms.RandomCrop.get_params(x[0], output_size=(w, h))
+        return [tf.crop(x_, *p) for x_ in x]
+
+
+if __name__ == "__main__":
+    # python3 -m imsegnet.transforms.tensor_transforms.random_crop
+    from ...datasets.moving_mnist import MovingMNIST
+    s = MovingMNIST(root_dir="./data", tensor_transforms=[RandomCrop()])
+    d, _ = next(iter(s))
+    print(d.shape)
